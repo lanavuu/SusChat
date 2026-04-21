f0edf64ca2f3504eb18fadf1d40103ed7766833c
Lana Vu <lanavu@csu.fullerton.edu>
2026-04-21 12:57:57 -0700

    a lot of pages, login function wrong but this all i got

--- functions.py
+++ functions.py
@@ -10,3 +10,12 @@ def createAccount(username, password):
     with open("account_management", "w") as f:
         json.dump(file, f)
 
+def login(username, password):
+    with open("account_management", "r") as f:
+        file = json.load(f)
+        
+    tries = 3
+    while tries != 0:
+        if file["name"] != username or file["password"] != password:
+            tries -= 1
+            print(f"Username or password is incorrect, please try again. You have {tries} left")
