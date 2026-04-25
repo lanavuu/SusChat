85ef28ed508a509d9ea3f7d926384f0f613a2c66
Lana Vu <lananghivu@gmail.com>
2026-04-24 19:02:24 -0700

    q

--- functions.py
+++ functions.py
@@ -1,39 +1,39 @@
 import json
 
 class account():
-    def __init__(self, username, password, file="account_management.json"):
-        self.username = username
-        self.password = password
+    def __init__(self, filename="account_management.json"):
+        self.file = filename
+        self.__account_list = []
 
-def create_account(username, password):
-    with open("account_management.json", "r") as f:
-        file = json.load(f)
+    def create_account(self, username, password):
+        with open("account_management.json", "r") as f:
+            file = json.load(f)
 
-    file["username"] = username
-    file["password"] = password
+        file["username"] = username
+        file["password"] = password
 
-    with open("account_managemen.json", "w") as f:
-        json.dump(file, f)
+        with open("account_managemen.json", "w") as f:
+            json.dump(file, f)
 
-def login(username, password):
-    with open("account_management.json", "r") as f:
-        file = json.load(f)
+    def login(self, username, password):
+        with open("account_management.json", "r") as f:
+            file = json.load(f)
 
-    tries = 3
-    logged_in = False
-    while tries != 0:
-        if file["username"] != username or file["password"] != password:
-            tries -= 1
-            print(f"Username or password is incorrect, please try again. You have {tries} left")
-        else:
-            logged_in = True
-            break
-    return logged_in
-    
-    
-def crew_list():
-    with open("account_management.json", "r") as f:
-        file = json.load(f)
+        tries = 3
+        logged_in = False
+        while tries != 0:
+            if file["username"] != username or file["password"] != password:
+                tries -= 1
+                print(f"Username or password is incorrect, please try again. You have {tries} left")
+            else:
+                logged_in = True
+                break
+        return logged_in
+        
+        
+    def crew_list(self):
+        with open("account_management.json", "r") as f:
+            file = json.load(f)
 
-    friends = list(file.values())
-    print(f"List of friends: {friends}")
+        friends = list(file.values())
+        print(f"List of friends: {friends}")
