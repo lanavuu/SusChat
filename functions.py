619adf77873a3b4379b55bea9327581bd7b69a0c
Lana Vu <lananghivu@gmail.com>
2026-05-03 16:48:31 -0700

    want to remove everythign before delete

--- functions.py
+++ functions.py
@@ -23,9 +23,13 @@ class account():
         
         with open(self.file, "r", encoding="utf-8") as f:
             file = json.load(f)
-
+        
         for user in file:
             if user['name'] == self.current_user['name']:
+                file.remove(user['friends'])
+                file.remove(user['sent friend requests'])
+                file.remove(user['received friend requests'])
+                file.remove(user['blocked'])
                 file.remove(user)
                 self.current_user = None
                 with open(self.file,"w", encoding="utf-8") as f:
